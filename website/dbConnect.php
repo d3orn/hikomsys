446b53f4cd63cade438a0668f2db78b56b0f806b
d3orn <dominique.rahm@gmail.com>
2014-02-09 14:54:07 +0100

    minor fix

--- website/dbConnect.php
+++ website/dbConnect.php
@@ -79,7 +79,9 @@
 
 			<script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
 			<script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
-			<script type="text/javascript" src="/hikomsys/min/?b=hikomsys/scripts/javascripts&amp;f=jstree/jquery.jstree.js,jstree/_lib/jquery.cookie.js,packageViewer.js"></script> 
+			<script src="/scripts/javascripts/jstree/jquery.jstree.js"></script>
+			<script src="/scripts/javascripts/jstree/_lib/jquery.cookie.js"></script>
+			<script type="text/javascript" src="/hikomsys/min/?b=hikomsys/scripts/javascripts&amp;f=packageViewer.js"></script> 
 	
 		</div>
 	</body>
