78c6611543f446fd22a6856bbd4a03d74bbfbb20
Dominique Rahm <dominique.rahm@gmail.com>
2014-03-20 17:20:53 +0100

    c36

--- website/hikomsys/app/views/users/userlist.blade.php
+++ website/hikomsys/app/views/users/userlist.blade.php
@@ -20,15 +20,14 @@
 					<td>{{ $user->firstname }}</td>
 					<td>{{ $user->email }}</td>
 					<td>
-						{{ Form::open(['route' => ['users.destroy' , $user->id]]) }}
-							{{ Form::hidden('_method', 'DELETE') }}
-							{{ Form::submit('Delete') }}
-						{{ Form::close() }}
-
 						{{ HTML::linkRoute('users.show', 'Inspect this User', [$user->id]) }}
 
 						@if(Auth::user()->username == 'd3orn')
 							{{ HTML::linkRoute('users.edit', 'Edit this User', [$user->id]) }}
+							[{ Form::open(['route' => ['users.destroy' , $user->id]]) }}
+								{{ Form::hidden('_method', 'DELETE') }}
+								{{ Form::submit('Delete') }}
+							{{ Form::close() }}
 						@endif
 					</td>
 				</tr>
