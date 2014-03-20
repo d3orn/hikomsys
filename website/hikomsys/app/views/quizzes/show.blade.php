271cb65ea0f40412ba04880a5a5ec46b28a73898
Dominique Rahm <dominique.rahm@gmail.com>
2014-03-20 15:21:22 +0100

    c9

--- website/hikomsys/app/views/quizzes/show.blade.php
+++ website/hikomsys/app/views/quizzes/show.blade.php
@@ -6,15 +6,8 @@
 
 @section('content')
 	<h2>Draw the dependencies</h2>
-	<p>Please draw the dependencies between the packages you selected previously. 
-		
-		<br>To do so, you are able to switch between moving mode <a aria-hidden="true" data-icon="&#xe601;" class="icon-expand"></a> and
-		
-		drawing mode <a aria-hidden="true" data-icon="&#xe602;" class="icon-loop"></a>. 
-		<br>In moving mode you are able to move around your packages freely, if you did already draw some dependencies they will just move along.
-		<br>Within drawing mode you can <b>click-and-drag</b> from one package to another to visualize the dependency between them.
-		An arrow starting on one packages signals that this packages depends on the other package at the end of the arrow.
-		<br> As soon as you are satisfied with your dependencies please submit your solution by clicking on the submit dependencies button below.
+	<p>
+		Please draw the dependencies between the packages you selected previously. 
 	</p>	
 	<div id="menu">
 		<div id="move" aria-hidden="true" data-icon="&#xe601;" class="icon-expand activatedIcon buttonlike"></div>
@@ -22,7 +15,13 @@
 		<div id="help" class="gradientBG buttonlike">Help</div>
 		<div id="submit" class="gradientBG buttonlike">Submit dependencies</div>
 	</div>
-	<div id="help_container">THIS IS JUST A FILLER</div>
+	<div id="help_container"><br>You are able to switch between moving mode <a aria-hidden="true" data-icon="&#xe601;" class="icon-expand"></a> and
+		
+		drawing mode <a aria-hidden="true" data-icon="&#xe602;" class="icon-loop"></a>. 
+		<br>In moving mode you are able to move around your packages freely, if you did already draw some dependencies they will just move along.
+		<br>Within drawing mode you can <b>click-and-drag</b> from one package to another to visualize the dependency between them.
+		An arrow starting on one packages signals that this packages depends on the other package at the end of the arrow.
+		<br> As soon as you are satisfied with your dependencies please submit your solution by clicking on the submit dependencies button below.</div>
 	<div id="container"></div>
 
 	{{ HTML::script('http://d3lp1msu2r81bx.cloudfront.net/kjs/js/lib/kinetic-v4.7.4.min.js') }}
