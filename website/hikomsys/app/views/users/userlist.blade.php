7168023057a93ed62e2961ac84bc154a82c3927c
Dominique Rahm <dominique.rahm@gmail.com>
2014-03-20 17:13:42 +0100

    c29

--- website/hikomsys/app/views/users/userlist.blade.php
+++ website/hikomsys/app/views/users/userlist.blade.php
@@ -20,12 +20,9 @@
 					<td>{{ $user->firstname }}</td>
 					<td>{{ $user->email }}</td>
 					<td>
-
-						<!-- delete the nerd (uses the destroy method DESTROY /nerds/{id} -->
-						<!-- we will add this later since its a little more complicated than the other two buttons -->
-						{{ Form::open(array('url' => 'nerds/' . $user->id, 'class' => 'pull-right')) }}
+						{{ Form::open(array('route' => 'users.destroy' . $user->id)) }}
 							{{ Form::hidden('_method', 'DELETE') }}
-							{{ Form::submit('Delete this Nerd', array('class' => 'btn btn-warning')) }}
+							{{ Form::submit('Delete') }}
 						{{ Form::close() }}
 
 						{{ HTML::linkRoute('users.show', 'Inspect this User', [$user->id]) }}
