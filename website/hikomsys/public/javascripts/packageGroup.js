eb3d0d2f06836f5209c61b30f4a97681ac1c523f
d3orn <dominique.rahm@gmail.com>
2014-03-23 14:16:42 +0100

    c111 	# Please enter the commit message for your changes. Lines starting 	# with '#' will be ignored, and an empty message aborts the commit. 	# On branch clean 	# Changes to be committed: 	#	modified:   public/javascripts/packageGroup.js 	#

--- website/hikomsys/public/javascripts/packageGroup.js
+++ website/hikomsys/public/javascripts/packageGroup.js
@@ -409,15 +409,16 @@ function PackageGroup(text, color, infos){
 				stage.draw();
 			})
 			from.on('mouseleave', function(event){
-				alert('leave');
 				this.setFill('black');
 				var index = this.getName();
 				var id = this.getId().replace('From', '');
 				var pack = findPackageById(id);
 				var to = pack.dependenciesInfoBox[index]['to'];
 
-				to.remove();
-				stage.draw();
+				if(to){
+					to.remove();
+					stage.draw();
+				}
 			})
 		}
 
