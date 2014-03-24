aff67a84c2a6b229bec08b80747ecbf55bb12f09
Dominique Rahm <dominique.rahm@gmail.com>
2014-03-24 22:40:54 +0100

    c243

--- website/hikomsys/app/views/users/login.blade.php
+++ website/hikomsys/app/views/users/login.blade.php
@@ -6,7 +6,7 @@
 		<p> If you are new please {{ HTML::linkRoute('users.create', 'Sign up') }} , else sign in below </p>
 	</div>
 	<div class="row"></div>
-		<div class="large-4 medium-4 columns">
+		<div class="large-5 medium-5 columns">
 			<h2>Login</h2>
 
 			{{ Form::open(['route'=>'sessions.store']) }}
@@ -22,8 +22,8 @@
 			    {{ Form::submit('Login', ['class'=>'submit button'])}}
 			{{ Form::close() }}
 		</div>
-		<div class="large-8 medium-8 columns">
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum, neque!</p>
+		<div class="large-7 medium-7 columns">
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum, sed, maiores, tempore itaque quaerat nisi aliquid inventore ducimus ipsa saepe sunt enim voluptate velit. Cupiditate, doloribus, harum laborum eligendi id numquam culpa dolor sunt fuga beatae tempora quisquam temporibus. Nemo, cumque, fugit, autem, quia ullam neque aperiam deleniti quas quos praesentium molestias eaque vitae dolore et veniam? Voluptatem, fugit, laborum, temporibus nesciunt alias est enim minus reiciendis qui culpa provident numquam ullam optio officia nisi possimus debitis rem. Laboriosam, amet, doloremque, hic dicta aperiam quis modi asperiores quam veritatis earum tempora non dolores excepturi aliquid quidem ratione eaque! Molestiae, delectus.</p>
 		</div>
 	</div>
 @stop
