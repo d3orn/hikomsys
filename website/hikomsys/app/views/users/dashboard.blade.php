b8962bf25e4beb3440da85d932741ca2ebec73bd
Dominique Rahm <dominique.rahm@gmail.com>
2014-03-24 23:04:25 +0100

    c250

--- website/hikomsys/app/views/users/dashboard.blade.php
+++ website/hikomsys/app/views/users/dashboard.blade.php
@@ -11,23 +11,31 @@
 		 
 		<p>Welcome back {{ Auth::user()->firstname }} on How I KnOw My SYStem.</p>
 
-		<p>Please enter a valid Github link below</p>
+		<div class="row">
 
-		{{ Form::open(['route'=>'projects.store', 'class' => 'col-md-4']) }}
-				
-				<div class="form-group">
-					{{ Form::label('url', 'Gitrepository URL:', ['class' => 'form-label']) }}
-					{{ Form::url('url', null, ['class' => 'form-control'])}}
-				</div>
+			<div class="large-5 medium-5 columns">
 
-				<div class="form-group">
-					{{ Form::label('projectName', 'Project name:',  ['class' => 'form-label']) }}
-					{{ Form::text('projectName', null, ['class' => 'form-control']) }}
-				</div>
+				<p>Please enter a valid Github link below</p>
 
-			{{ Form::submit('Submit Repository', ['class'=>'submit btn btn-primary']) }}
+				{{ Form::open(['route'=>'projects.store', 'class' => 'col-md-4']) }}
+						
+						<div class="form-group">
+							{{ Form::label('url', 'Gitrepository URL:', ['class' => 'form-label']) }}
+							{{ Form::url('url', null, ['class' => 'form-control'])}}
+						</div>
 
-		{{ Form::close() }}
+						<div class="form-group">
+							{{ Form::label('projectName', 'Project name:',  ['class' => 'form-label']) }}
+							{{ Form::text('projectName', null, ['class' => 'form-control']) }}
+						</div>
+
+					{{ Form::submit('Submit Repository', ['class'=>'submit btn btn-primary']) }}
+
+				{{ Form::close() }}
+
+			</div>
+
+		</div>
 
 	</div>
 
