6d0f93506eadae2be2a5dbb88a4638f7b3d12f69
Dominique Rahm <dominique.rahm@gmail.com>
2014-03-24 20:37:30 +0100

    c217

--- website/hikomsys/app/views/users/login.blade.php
+++ website/hikomsys/app/views/users/login.blade.php
@@ -6,10 +6,10 @@
 		<p> If you are new please {{ HTML::linkRoute('users.create', 'Sign up') }} , else sign in below </p>
 	</div>
 	<div class="row"></div>
-		<div class="large-8 columns">
+		<div class="large-4 columns">
 			<h2>Login</h2>
 
-			{{ Form::open(['route'=>'sessions.store', 'class' => 'large-6 columns']) }}
+			{{ Form::open(['route'=>'sessions.store']) }}
 			  
 					<div class="">
 						{{ Form::label('username', 'Username') }}
