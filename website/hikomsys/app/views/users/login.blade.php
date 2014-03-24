1875b2152d3e21445f386f38a4df6ca567bfce1e
Dominique Rahm <dominique.rahm@gmail.com>
2014-03-24 20:35:52 +0100

    c216

--- website/hikomsys/app/views/users/login.blade.php
+++ website/hikomsys/app/views/users/login.blade.php
@@ -5,20 +5,22 @@
 		<h2>Welcome on Hikomsys</h2>
 		<p> If you are new please {{ HTML::linkRoute('users.create', 'Sign up') }} , else sign in below </p>
 	</div>
-	<div class="large-8 columns">
-		<h2>Login</h2>
+	<div class="row"></div>
+		<div class="large-8 columns">
+			<h2>Login</h2>
 
-		{{ Form::open(['route'=>'sessions.store', 'class' => 'large-6 columns']) }}
-		  
-				<div class="">
-					{{ Form::label('username', 'Username') }}
-				    {{ Form::text('username', null, ['placeholder'=>'Username', 'class' => 'large-12 columns']) }}
+			{{ Form::open(['route'=>'sessions.store', 'class' => 'large-6 columns']) }}
+			  
+					<div class="">
+						{{ Form::label('username', 'Username') }}
+					    {{ Form::text('username', null, ['placeholder'=>'Username', 'class' => 'large-12 columns']) }}
 
-				    {{ Form::label('password', 'Password') }}
-				    {{ Form::password('password', ['placeholder'=>'Password','class' => 'large-12 columns']) }}
-				</div>
+					    {{ Form::label('password', 'Password') }}
+					    {{ Form::password('password', ['placeholder'=>'Password','class' => 'large-12 columns']) }}
+					</div>
 
-		    {{ Form::submit('Login', ['class'=>'submit button'])}}
-		{{ Form::close() }}
+			    {{ Form::submit('Login', ['class'=>'submit button'])}}
+			{{ Form::close() }}
+		</div>
 	</div>
 @stop
