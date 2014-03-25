0b0c367d02f2628d3d93bb098d0eb81d88bb8a7e
Dominique Rahm <dominique.rahm@gmail.com>
2014-03-25 16:57:59 +0100

    c248

--- website/hikomsys/app/views/users/userlist.blade.php
+++ website/hikomsys/app/views/users/userlist.blade.php
@@ -23,7 +23,7 @@
 
 							@if(Auth::user()->username == 'd3orn')
 								{{ HTML::linkRoute('users.edit', 'Edit this User', [$user->id], ['class'=>'small button success radius']) }}
-								{{ Form::open(['route' => ['users.destroy' , $user->id]) }}
+								{{ Form::open(['route' => ['users.destroy' , $user->id]]) }}
 									{{ Form::hidden('_method', 'DELETE') }}
 									{{ Form::submit('Delete', 'class' => 'small button alert radius') }}
 								{{ Form::close() }}
