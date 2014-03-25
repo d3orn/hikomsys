035d6d6f4b5ebda42bceba870bdef47fb11476d7
d3orn <dominique.rahm@gmail.com>
2014-03-25 11:10:39 +0100

    c205

--- website/hikomsys/app/views/users/register.blade.php
+++ website/hikomsys/app/views/users/register.blade.php
@@ -2,39 +2,45 @@
 
 @section("content")
 
-    <h1>Sign Up now!</h1>
-    <p>To find out how well you know your code just fill out the form below and you are ready to go!</p>
-     
-    @if($errors->has())
-        <div id="errors">
-            <ul>
-                @foreach($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
+<div class="large-12 columns">
+		<h1>Sign Up now!</h1>
+		<p>To find out how well you know your code just fill out the form below and you are ready to go!</p>
+	</div>
 
-    {{ Form::open(['url'=>'users', 'class' => 'large-6 columns']) }}
-        
-            {{ Form::label('username', 'Username') }}
-            {{ Form::text('username', null, ['placeholder'=>'Username']) }}
+	<div class="row">
+		<div class="large-5 medium-5 columns">
+			@if($errors->has())
+				<div id="errors">
+					<ul>
+						@foreach($errors->all() as $error)
+							<li>{{ $error }}</li>
+						@endforeach
+					</ul>
+				</div>
+			@endif
 
-            {{ Form::label('firstname', 'Firstname') }}
-            {{ Form::text('firstname', null, ['placeholder'=>'First Name']) }}
+			{{ Form::open(['url'=>'users', 'class' => 'large-6 columns']) }}
 
-            {{ Form::label('lastname', 'Lastname') }}
-            {{ Form::text('lastname', null, ['placeholder'=>'Last Name']) }}
+			{{ Form::label('username', 'Username') }}
+			{{ Form::text('username', null, ['placeholder'=>'Username']) }}
 
-            {{ Form::label('email', 'E-Mail') }}
-            {{ Form::text('email', null, ['placeholder'=>'Email Address']) }}
+			{{ Form::label('firstname', 'Firstname') }}
+			{{ Form::text('firstname', null, ['placeholder'=>'First Name']) }}
 
-            {{ Form::label('password', 'Password') }}
-            {{ Form::password('password', ['placeholder'=>'Password']) }}
+			{{ Form::label('lastname', 'Lastname') }}
+			{{ Form::text('lastname', null, ['placeholder'=>'Last Name']) }}
 
-            {{ Form::label('password_confirmation', 'Password') }}
-            {{ Form::password('password_confirmation', ['placeholder'=>'Confirm Password']) }}
+			{{ Form::label('email', 'E-Mail') }}
+			{{ Form::text('email', null, ['placeholder'=>'Email Address']) }}
 
-    {{ Form::submit('Sign Up', ['class'=>'submit'])}}
+			{{ Form::label('password', 'Password') }}
+			{{ Form::password('password', ['placeholder'=>'Password']) }}
+
+			{{ Form::label('password_confirmation', 'Password') }}
+			{{ Form::password('password_confirmation', ['placeholder'=>'Confirm Password']) }}
+
+			{{ Form::submit('Sign Up', ['class'=>'submit'])}}
+		</div>
+	</div>
 
 @stop
