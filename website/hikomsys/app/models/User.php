4509d99727657a8db30d3ce659d92d0fe53bda81
d3orn <dominique.rahm@gmail.com>
2014-03-25 11:52:38 +0100

    c212

--- website/hikomsys/app/models/User.php
+++ website/hikomsys/app/models/User.php
@@ -8,8 +8,8 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 	protected $guarded = array('id', 'created_at', 'updated_at');
 
 	public static $rules = array(
-   		'firstname'=>'required|alpha|min:2',
-    	'lastname'=>'required|alpha|min:2',
+   		'firstname'=>'alpha|min:2',
+    	'lastname'=>'alpha|min:2',
     	'email'=>'required|email|unique:users',
     	'username'=>'required|alpha_num|unique:users',
     	'password'=>'required|alpha_num|between:6,12|confirmed',
