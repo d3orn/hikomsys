9b1443e7b932ff983da870b79fbe7a289092696d
d3orn <dominique.rahm@gmail.com>
2014-03-27 11:48:45 +0100

    c308

--- website/hikomsys/app/views/layouts/default.blade.php
+++ website/hikomsys/app/views/layouts/default.blade.php
@@ -120,14 +120,13 @@
 								</a>	
 								<p>a clean, innovative, free open-source Smalltalk-inspired environment</p>
 							</div>
-							<div class="vertDivider"></div>
-							<div class="medium-5 columns">
+							<div class="medium-5 columns column">
 								<p>a platform for software and data analysis</p>
 								<a href="http://www.moosetechnology.org/" target="_blank">	
 									{{ HTML::image('foundation/img/mooseTech.fw.png', 'Moose Technology Logo') }}
 								</a>
 							</div>	
-							<div class="medium-3 columns left">
+							<div class="medium-3 columns column left">
 							<a href="http://www.d3orn.ch" target="_blank">
 								{{ HTML::image('foundation/img/d3orn.fw.png', 'd3orn Logo') }}
 							</a>	
