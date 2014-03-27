55029b58d14e022570c8dd89e5bf77fd67aa47a1
d3orn <dominique.rahm@gmail.com>
2014-03-27 10:09:35 +0100

    c267

--- website/hikomsys/app/views/users/edit.blade.php
+++ website/hikomsys/app/views/users/edit.blade.php
@@ -6,27 +6,29 @@
 @section("content")
     
     <div class="row">
-		<h1> Edit your profile </h1>
+    	<div class="large-5 medium-5 columns">
+			<h1> Edit your profile </h1>
 
-	     {{ Form::model($user, ['method' => 'PATCH', 'route' => ['users.update', $user->id], 'class' => 'large-5 medium-5 columns']) }}
+		     {{ Form::model($user, ['method' => 'PATCH', 'route' => ['users.update', $user->id]]) }}
 
-	            {{ Form::label('firstname', 'First Name:') }}
-	            {{ Form::text('firstname', null, ['class' => 'form-control']) }}
+		            {{ Form::label('firstname', 'First Name:') }}
+		            {{ Form::text('firstname', null, ['class' => 'form-control']) }}
 
-	            {{ Form::label('lastname', 'Last Name:') }}
-	            {{ Form::text('lastname', null, ['class' => 'form-control']) }}
+		            {{ Form::label('lastname', 'Last Name:') }}
+		            {{ Form::text('lastname', null, ['class' => 'form-control']) }}
 
-	            {{ Form::label('email', 'E-Mail') }}
-	            {{ Form::text('email', null, ['class' => 'form-control']) }}
+		            {{ Form::label('email', 'E-Mail') }}
+		            {{ Form::text('email', null, ['class' => 'form-control']) }}
 
-	            {{ Form::label('username', 'Username') }}
-	            {{ Form::text('username', null, ['class' => 'form-control']) }}
+		            {{ Form::label('username', 'Username') }}
+		            {{ Form::text('username', null, ['class' => 'form-control']) }}
 
-	            {{ Form::label('description', 'Description') }}
-	            {{ Form::textarea('description', null, ['class' => 'form-control']) }}
+		            {{ Form::label('description', 'Description') }}
+		            {{ Form::textarea('description', null, ['class' => 'form-control']) }}
 
-			{{ Form::submit('Update Profile') }}
-	    {{ Form::close() }}
+				{{ Form::submit('Update Profile') }}
+		    {{ Form::close() }}
+	    </div>
 	</div>
 
 @stop
