edea497046b9312113fa559d9cdb64d7234daa12
d3orn <dominique.rahm@gmail.com>
2014-03-27 10:08:25 +0100

    c266

--- website/hikomsys/app/views/users/edit.blade.php
+++ website/hikomsys/app/views/users/edit.blade.php
@@ -4,37 +4,29 @@
 @stop
 
 @section("content")
+    
+    <div class="row">
+		<h1> Edit your profile </h1>
 
-	<h1> Edit your profile </h1>
+	     {{ Form::model($user, ['method' => 'PATCH', 'route' => ['users.update', $user->id], 'class' => 'large-5 medium-5 columns']) }}
 
-     {{ Form::model($user, ['method' => 'PATCH', 'route' => ['users.update', $user->id], 'class' => 'col-md-4']) }}
+	            {{ Form::label('firstname', 'First Name:') }}
+	            {{ Form::text('firstname', null, ['class' => 'form-control']) }}
 
-        <div class="form-group">
-            {{ Form::label('firstname', 'First Name:', ['class' => 'form-label']) }}
-            {{ Form::text('firstname', null, ['class' => 'form-control']) }}
-        </div>
+	            {{ Form::label('lastname', 'Last Name:') }}
+	            {{ Form::text('lastname', null, ['class' => 'form-control']) }}
 
-        <div class="form-group">
-            {{ Form::label('lastname', 'Last Name:', ['class' => 'form-label']) }}
-            {{ Form::text('lastname', null, ['class' => 'form-control']) }}
-        </div>
+	            {{ Form::label('email', 'E-Mail') }}
+	            {{ Form::text('email', null, ['class' => 'form-control']) }}
 
-        <div class="form-group">
-            {{ Form::label('email', 'E-Mail', ['class' => 'form-label']) }}
-            {{ Form::text('email', null, ['class' => 'form-control']) }}
-        </div>
+	            {{ Form::label('username', 'Username') }}
+	            {{ Form::text('username', null, ['class' => 'form-control']) }}
 
-        <div class="form-group">
-            {{ Form::label('username', 'Username', ['class' => 'form-label']) }}
-            {{ Form::text('username', null, ['class' => 'form-control']) }}
-        </div>
+	            {{ Form::label('description', 'Description') }}
+	            {{ Form::textarea('description', null, ['class' => 'form-control']) }}
 
-        <div class="form-group">
-            {{ Form::label('description', 'Description', ['class' => 'form-label']) }}
-            {{ Form::textarea('description', null, ['class' => 'form-control']) }}
-        </div>
-
-		{{ Form::submit('Update Profile') }}
-    {{ Form::close() }}
+			{{ Form::submit('Update Profile') }}
+	    {{ Form::close() }}
+	</div>
 
 @stop
