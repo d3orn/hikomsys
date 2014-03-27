ed9d64ce6438a1c13f94196c8a6ac8eaff626a4a
d3orn <dominique.rahm@gmail.com>
2014-03-27 10:24:38 +0100

    c272

--- website/hikomsys/app/views/users/edit.blade.php
+++ website/hikomsys/app/views/users/edit.blade.php
@@ -4,60 +4,66 @@
 @stop
 
 @section("content")
-    
-    <div class="row">
-    	<div class="medium-10 columns">
+
+	<div class="row">
+		<div class="medium-10 columns">
 			<h1> Edit your profile </h1>
 		</div>
 	</div>
-	
-		     {{ Form::model($user, ['method' => 'PATCH', 'route' => ['users.update', $user->id]]) }}
 
-				<div class="row">
-		        	<div class="medium-3 columns">   	
-		            	{{ Form::label('firstname', 'First Name:', ['class' => 'left inline']) }}
-		            </div>
-		         	<div class="medium-9 columns">
-		           		{{ Form::text('firstname') }}
-		            </div>
-				</div>
+	<div class="row">
+		
+		{{ Form::model($user, ['method' => 'PATCH', 'route' => ['users.update', $user->id]]) }}
 
-				<div class="row">
-		        	<div class="medium-3 columns">  
-		           		{{ Form::label('lastname', 'Last Name:', ['class' => 'left inline']) }}
-		            </div>
-		            <div class="medium-9 columns">
-		            	{{ Form::text('lastname') }}
-		            </div>
+			<div class="row">
+				<div class="medium-3 columns">   	
+					{{ Form::label('firstname', 'First Name:', ['class' => 'left inline']) }}
 				</div>
+			 	<div class="medium-9 columns">
+			   		{{ Form::text('firstname') }}
+				</div>
+			</div>
 
-				<div class="row">
-		        	<div class="medium-3 columns">  
-		            	{{ Form::label('email', 'E-Mail', ['class' => 'left inline']) }}
-		            </div>
-		            <div class="medium-9 columns">
-		            	{{ Form::text('email') }}
-		            </div>
+			<div class="row">
+				<div class="medium-3 columns">  
+			   		{{ Form::label('lastname', 'Last Name:', ['class' => 'left inline']) }}
 				</div>
+				<div class="medium-9 columns">
+					{{ Form::text('lastname') }}
+				</div>
+			</div>
 
-				<div class="row">
-		        	<div class="medium-3 columns">  
-		            	{{ Form::label('username', 'Username', ['class' => 'left inline']) }}
-		            </div>
-		            <div class="medium-9 columns">
-		            	{{ Form::text('username') }}
-		            </div>
+			<div class="row">
+				<div class="medium-3 columns">  
+					{{ Form::label('email', 'E-Mail', ['class' => 'left inline']) }}
 				</div>
-	
-				<div class="row">
-		        	<div class="medium-3 columns">  
-		            	{{ Form::label('description', 'Description', ['class' => 'left inline']) }}
-		            </div>
-		            <div class="medium-9 columns">
-		           		{{ Form::textarea('description') }}
-		            </div>
+				<div class="medium-9 columns">
+					{{ Form::text('email') }}
 				</div>
+			</div>
 
-				{{ Form::submit('Update Profile', ['class'=>'submit button']) }}
-		    {{ Form::close() }}
+			<div class="row">
+				<div class="medium-3 columns">  
+					{{ Form::label('username', 'Username', ['class' => 'left inline']) }}
+				</div>
+				<div class="medium-9 columns">
+					{{ Form::text('username') }}
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="medium-3 columns">  
+					{{ Form::label('description', 'Description', ['class' => 'left inline']) }}
+				</div>
+				<div class="medium-9 columns">
+			   		{{ Form::textarea('description') }}
+				</div>
+			</div>
+
+			{{ Form::submit('Update Profile', ['class'=>'submit button']) }}
+
+		{{ Form::close() }}
+
+	</div>
+
 @stop
