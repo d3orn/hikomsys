44339e899ba491256f7e549dc9d68f206dfc06e4
d3orn <dominique.rahm@gmail.com>
2014-03-27 10:21:35 +0100

    c271

--- website/hikomsys/app/views/users/edit.blade.php
+++ website/hikomsys/app/views/users/edit.blade.php
@@ -17,22 +17,46 @@
 		        	<div class="medium-3 columns">   	
 		            	{{ Form::label('firstname', 'First Name:', ['class' => 'left inline']) }}
 		            </div>
-		            <div class="medium-9 columns">
+		         	<div class="medium-9 columns">
 		           		{{ Form::text('firstname') }}
 		            </div>
 				</div>
+
+				<div class="row">
+		        	<div class="medium-3 columns">  
+		           		{{ Form::label('lastname', 'Last Name:', ['class' => 'left inline']) }}
+		            </div>
+		            <div class="medium-9 columns">
+		            	{{ Form::text('lastname') }}
+		            </div>
+				</div>
+
+				<div class="row">
+		        	<div class="medium-3 columns">  
+		            	{{ Form::label('email', 'E-Mail', ['class' => 'left inline']) }}
+		            </div>
+		            <div class="medium-9 columns">
+		            	{{ Form::text('email') }}
+		            </div>
+				</div>
+
+				<div class="row">
+		        	<div class="medium-3 columns">  
+		            	{{ Form::label('username', 'Username', ['class' => 'left inline']) }}
+		            </div>
+		            <div class="medium-9 columns">
+		            	{{ Form::text('username') }}
+		            </div>
+				</div>
 	
-		            {{ Form::label('lastname', 'Last Name:') }}
-		            {{ Form::text('lastname') }}
-
-		            {{ Form::label('email', 'E-Mail') }}
-		            {{ Form::text('email') }}
-
-		            {{ Form::label('username', 'Username') }}
-		            {{ Form::text('username') }}
-
-		            {{ Form::label('description', 'Description') }}
-		            {{ Form::textarea('description') }}
+				<div class="row">
+		        	<div class="medium-3 columns">  
+		            	{{ Form::label('description', 'Description', ['class' => 'left inline']) }}
+		            </div>
+		            <div class="medium-9 columns">
+		           		{{ Form::textarea('description') }}
+		            </div>
+				</div>
 
 				{{ Form::submit('Update Profile', ['class'=>'submit button']) }}
 		    {{ Form::close() }}
