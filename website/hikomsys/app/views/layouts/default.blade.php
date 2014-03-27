2a6a9f7b6192ec120babda500f8bbb63f2181ced
d3orn <dominique.rahm@gmail.com>
2014-03-27 10:55:44 +0100

    c284

--- website/hikomsys/app/views/layouts/default.blade.php
+++ website/hikomsys/app/views/layouts/default.blade.php
@@ -113,13 +113,13 @@
 			<footer class="bottom-bar">	
 				<section class="Links">
 					<a href="http://www.pharo-project.org/home" target="_blank">
-						{{ HTML::image('foundation/img/pharo.png', 'Smalltalk Pharo Logo') }}
+						{{ HTML::image('foundation/img/pharo.fw.png', 'Smalltalk Pharo Logo') }}
 					</a>	
 					<a href="http://www.moosetechnology.org/" target="_blank">
-						{{ HTML::image('foundation/img/mooseTech.png', 'Moose Technology Logo') }}
+						{{ HTML::image('foundation/img/mooseTech.fw.png', 'Moose Technology Logo') }}
 					</a>	
 					<a href="http://www.d3orn.ch" target="_blank">
-						{{ HTML::image('foundation/img/d3orn.png', 'd3orn Logo') }}
+						{{ HTML::image('foundation/img/d3orn.fw.png', 'd3orn Logo') }}
 					</a>		
 				</section>
 				<section class="footertext">
