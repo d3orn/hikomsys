16e15b89ffe1944d15ab2265ac32166ccd486da5
Dominique Rahm <dominique.rahm@gmail.com>
2014-03-28 12:09:32 +0100

    c236

--- website/hikomsys/app/views/projects/view.blade.php
+++ website/hikomsys/app/views/projects/view.blade.php
@@ -6,20 +6,22 @@
 
 
 @section("content")
-	<h1>{{ $project->name }}</h1>  
+	<div class="row">
+		<h1>{{ $project->name }}</h1>  
 
-	<p> Please select the packages you think are most important for your project. Your knowledge will be tested base on the selection you made. </p> 
-	{{ Form::open(['route' => 'quizzes.store', 'id' => 'package_viewer' , 'class' => 'col-md-4']) }}
+		<p> Please select the packages you think are most important for your project. Your knowledge will be tested base on the selection you made. </p> 
+		{{ Form::open(['route' => 'quizzes.store', 'id' => 'package_viewer' , 'class' => 'col-md-4']) }}
 
-		<div id="package_list">
-			<ul>
-				{{ Helpers::recursiveTree($cursor, $collection) }}
-			</ul>
-		</div>
-		{{ Form::hidden('project_id', $project->id) }}
-		{{Form::submit('Start Quiz', ['class'=>'submit btn btn-primary'])}}
+			<div id="package_list">
+				<ul>
+					{{ Helpers::recursiveTree($cursor, $collection) }}
+				</ul>
+			</div>
+			{{ Form::hidden('project_id', $project->id) }}
+			{{Form::submit('Start Quiz', ['class'=>'submit btn btn-primary'])}}
 
-	{{Form::close()}}
+		{{Form::close()}}
+	</div>
 
 @stop
 
