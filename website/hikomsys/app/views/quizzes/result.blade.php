cfb8e9c9c17867509e667c932e9e4d2056879a8d
Dominique Rahm <dominique.rahm@gmail.com>
2014-03-28 20:27:15 +0100

    c297

--- website/hikomsys/app/views/quizzes/result.blade.php
+++ website/hikomsys/app/views/quizzes/result.blade.php
@@ -40,33 +40,3 @@
 	{{ HTML::script('javascripts/packageGroup.js') }}
 	{{ HTML::script('javascripts/kineticResults.js') }}
 @stop
-
-	<div class="row">
-		<div class="medium-12 columns">
-			<h2>Draw the dependencies</h2>
-			<p>
-				Please draw the dependencies between the packages you selected previously. 
-			</p>	
-		</div>
-	</div>
-	
-
-	<div class="row">
-		<div class="medium-12 columns">
-			<div id="container"></div>
-
-			<input id="quizId" type="hidden" value={{ $quizId }} >
-
-			<!-- Size Classes: [small medium large xlarge expand] -->
-			<div id="myModal" class="reveal-modal" data-reveal>
-				<h2>Help</h2>
-				<p>You are able to switch between moving mode <a aria-hidden="true" data-icon="&#xe601;" class="icon-expand"></a> and
-				drawing mode <a aria-hidden="true" data-icon="&#xe602;" class="icon-loop"></a>. </p>
-				<p>In moving mode you are able to move around your packages freely, if you did already draw some dependencies they will just move along.</p>
-				<p>Within drawing mode you can <b>click-and-drag</b> from one package to another to visualize the dependency between them.
-				An arrow starting on one packages signals that this packages depends on the other package at the end of the arrow.</p>
-				<p> As soon as you are satisfied with your dependencies please submit your solution by clicking on the submit dependencies button.</p>
-				<a class="close-reveal-modal">&#215;</a>
-			</div>	
-		</div>
-	</div>
