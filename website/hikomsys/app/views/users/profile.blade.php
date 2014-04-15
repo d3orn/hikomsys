b0dc141cb321baa54b01b238d93198d4ce4bed41
Dominique Rahm <dominique.rahm@gmail.com>
2014-04-15 17:05:05 +0200

    c353

--- website/hikomsys/app/views/users/profile.blade.php
+++ website/hikomsys/app/views/users/profile.blade.php
@@ -9,20 +9,20 @@
 
      {{ Form::model($user, ['method' => 'PATCH', 'route' => ['users.update', $user->id]]) }}
 
-            {{ Form::label('firstname', 'First Name:', array('class' => 'address')) }}
-            {{ Form::text('firstname') }}
+        {{ Form::label('firstname', 'First Name:', array('class' => 'address')) }}
+        {{ Form::text('firstname') }}
 
-            {{ Form::label('lastname', 'Last Name:', array('class' => 'address')) }}
-            {{ Form::text('lastname') }}
+        {{ Form::label('lastname', 'Last Name:', array('class' => 'address')) }}
+        {{ Form::text('lastname') }}
 
-            {{ Form::label('email', 'E-Mail', array('class' => 'address')) }}
-            {{ Form::text('email') }}
+        {{ Form::label('email', 'E-Mail', array('class' => 'address')) }}
+        {{ Form::text('email') }}
 
-            {{ Form::label('username', 'Username', array('class' => 'address')) }}
-            {{ Form::text('username') }}
+        {{ Form::label('username', 'Username', array('class' => 'address')) }}
+        {{ Form::text('username') }}
 
-            {{ Form::label('description', 'Description', array('class' => 'address')) }}
-            {{ Form::textarea('description') }}
+        {{ Form::label('description', 'Description', array('class' => 'address')) }}
+        {{ Form::textarea('description') }}
 
 		{{ Form::submit('Update Profile') }}
     {{ Form::close() }}
--- website/hikomsys/app/views/users/show.blade.php
+++ website/hikomsys/app/views/users/show.blade.php
@@ -7,9 +7,23 @@
 
 	<h2> {{ $user->username }} </h2>
 		
-		<label for="firstname">First Name:</label>
-		<input type="text">{{ $user->firstname }}</input>
-		
+     {{ Form::model($user, ['method' => 'PATCH', 'route' => ['users.update', $user->id]]) }}
 
+        {{ Form::label('firstname', 'First Name:', array('class' => 'address')) }}
+        {{ Form::text('firstname', array('disabled')) }}
+
+        {{ Form::label('lastname', 'Last Name:', array('class' => 'address')) }}
+        {{ Form::text('lastname', array('disabled')) }}
+
+        {{ Form::label('email', 'E-Mail', array('class' => 'address')) }}
+        {{ Form::text('email', array('disabled') }}
+
+        {{ Form::label('username', 'Username', array('class' => 'address')) }}
+        {{ Form::text('username', array('disabled')) }}
+
+        {{ Form::label('description', 'Description', array('class' => 'address')) }}
+        {{ Form::textarea('description', array('disabled')) }}
+
+    {{ Form::close() }}
 
 @stop
