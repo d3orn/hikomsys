2ce5b26d101607d8bde12adf49281c0564ed3bd3
d3orn <dominique.rahm@gmail.com>
2014-05-12 23:53:21 +0200

    c431

--- website/hikomsys/public/javascripts/packageViewer.js
+++ website/hikomsys/public/javascripts/packageViewer.js
@@ -18,14 +18,13 @@ $(document).ready(function(){
 			items : function(node){ 
 				return{
 					"expand" : {
-						label: "Expand", 
+						label: "Expand all children", 
 						action: function(){ console.log(node)}
 					}
-					// },
-					// "close" : {
-					// 	label: "Close", 
-					// 	"action" : console.log(node)
-					// }
+					"close" : {
+						label: "Close and deselect all children", 
+						action: function(){ closeAndDeselectChildren(node.id)}
+					}
 				}
 			}
 
@@ -48,23 +47,24 @@ $('form').submit(function (e) {
 	$(this).append($hidden);
 });
 
-$("#package_list").bind("dblclick.jstree", function (event) {
-	var node = $(event.target).closest("li");
-	var data = node.data("jstree");
-	var id = node[0].id;
-	uncheckChilds(id);
-	if(node.hasClass('jstree-open')){
-		$("#package_list").jstree("close_all",id);
- 	}
-	else{
-		$("#package_list").jstree("open_all",id);
-	}
-});
+// $("#package_list").bind("dblclick.jstree", function (event) {
+// 	var node = $(event.target).closest("li");
+// 	var data = node.data("jstree");
+// 	var id = node[0].id;
+// 	uncheckChilds(id);
+// 	if(node.hasClass('jstree-open')){
+// 		$("#package_list").jstree("close_all",id);
+//  	}
+// 	else{
+// 		$("#package_list").jstree("open_all",id);
+// 	}
+// });
 
-function uncheckChilds(id){
+function closeAndDeselectChildren(id){
 	$("#package_list").jstree("deselect_node",id);
+	$("#package_list").jstree("close_all",id);
 	var children = $("#package_list").jstree("get_children_dom", id);
 	for(var i = 0; i < children.length;	 i++){
-		uncheckChilds(children[i].id);
+		closeAndDeselectChildren(children[i].id);
 	}
 }
