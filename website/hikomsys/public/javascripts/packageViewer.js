b56e60d15292c4b8671bc70f06bf111ed5a36dea
d3orn <dominique.rahm@gmail.com>
2014-05-12 11:08:42 +0200

    c378

--- website/hikomsys/public/javascripts/packageViewer.js
+++ website/hikomsys/public/javascripts/packageViewer.js
@@ -38,6 +38,6 @@ $('form').submit(function (e) {
 $("#package_list").bind("dblclick.jstree", function (event) {
    var node = $(event.target).closest("li");
    var data = node.data("jstree");
-   alert('test');
+   alert(node);
    // Do my action
 });
