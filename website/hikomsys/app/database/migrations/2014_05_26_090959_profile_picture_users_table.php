66d212b6136ef35440405e1df9562dff26edbf64
d3orn <dominique.rahm@gmail.com>
2014-05-26 11:11:53 +0200

    c497

--- website/hikomsys/app/database/migrations/2014_05_26_090959_profile_picture_users_table.php
+++ website/hikomsys/app/database/migrations/2014_05_26_090959_profile_picture_users_table.php
@@ -12,7 +12,10 @@ class ProfilePictureUsersTable extends Migration {
 	 */
 	public function up()
 	{
-		//
+        Schema::table('users', function($table)
+        {
+            $table->string('profile_picture');
+        });
 	}
 
 	/**
@@ -22,7 +25,10 @@ class ProfilePictureUsersTable extends Migration {
 	 */
 	public function down()
 	{
-		//
+        Schema::table('users', function($table)
+        {
+            $table->dropColumn('profile_picture');
+        });
 	}
 
 }
