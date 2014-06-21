9718ca80f6da8c5af636eb4e0f372fd2ce3278a2
d3orn <dominique.rahm@gmail.com>
2014-06-21 09:33:43 +0200

    c538

--- website/hikomsys/app/models/Project.php
+++ website/hikomsys/app/models/Project.php
@@ -2,11 +2,15 @@
 
 class Project extends Eloquent {
 	
+	public static $rules = array(
+   		'name'=>'alpha_num|between:3,50'
+    );
+
 	/**
 	 * The database table used by the model.
 	 *
 	 * @var string
 	 */
 	protected $table = 'projects';
-
 }
+
