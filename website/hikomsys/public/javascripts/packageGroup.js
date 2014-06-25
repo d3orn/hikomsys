5efa37eac3e1ac9c9d7b2f2ca8e81600e018685d
Dominique Rahm <dominique.rahm@gmail.com>
2014-06-25 15:52:44 +0200

    k34

--- website/hikomsys/public/javascripts/packageGroup.js
+++ website/hikomsys/public/javascripts/packageGroup.js
@@ -189,7 +189,7 @@ function PackageGroup(text, color, infos){
 				stage.draw();
 			}
 			if(drawingEnabled){
-				if(packages.length > 0 && this.getId() === packages[0].text){return;}
+				if(typeof firstSelectedPackage !== 'undefined' && this.getId() === packages[0].text){return;}
 				var pack = findPackageById(this.getId());
 				pack.highlight.remove(); 
 				packageLayer.draw();
