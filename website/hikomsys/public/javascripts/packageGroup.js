4d5b07af16274e4683eb432b881f8a1af43726fd
Dominique Rahm <dominique.rahm@gmail.com>
2014-06-26 14:30:47 +0200

    8# Please enter the commit message for your changes. Lines starting

--- website/hikomsys/public/javascripts/packageGroup.js
+++ website/hikomsys/public/javascripts/packageGroup.js
@@ -142,17 +142,16 @@ function PackageGroup(text, color, infos){
 	};
 
 	this.addEventListener = function(){
-		this.group.on('click', function(evt){
+		this.group.on('click', function(event){
 			if(event.button == 2){console.log('right mouse button')}
 		})
 
-		this.group.on('mousedown', function (event) {
-			console.log(event['button']);
+		this.group.on('mousedown', function(event) {
 			if(drawingEnabled){
 				mouseDownOnPackage(this);
 			}
 		}, false);
-		this.group.on('mouseup', function (event) {
+		this.group.on('mouseup', function(event) {
 			if(drawingEnabled){
 				mouseUpOnPackage(this);}
 		}, false);
