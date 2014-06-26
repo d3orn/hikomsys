24ccfa6b1dd372343b92b5310b2dff97cde61ed4
Dominique Rahm <dominique.rahm@gmail.com>
2014-06-26 14:23:18 +0200

    gh4

--- website/hikomsys/public/javascripts/packageGroup.js
+++ website/hikomsys/public/javascripts/packageGroup.js
@@ -142,11 +142,8 @@ function PackageGroup(text, color, infos){
 	};
 
 	this.addEventListener = function(){
-		this.group.on('click', function(e){
-			if(e.button == 2){console.log('right mouse button')}
-		})
-
 		this.group.on('mousedown', function (event) {
+			if(event.button == 2){console.log('right mouse button')}
 			if(drawingEnabled){
 				mouseDownOnPackage(this,event);
 			}
