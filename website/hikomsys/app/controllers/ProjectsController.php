93c5681519e1024cda6ce436750b113376c93905
d3orn <dominique.rahm@gmail.com>
2014-07-05 14:08:05 +0200

    s15

--- website/hikomsys/app/controllers/ProjectsController.php
+++ website/hikomsys/app/controllers/ProjectsController.php
@@ -154,8 +154,8 @@ class ProjectsController extends BaseController {
 		return $project->id;
 	}
 
-	public function ranking($id){
-		return View::make('projects.ranking', compact($id));
+	public function ranking($projectid){
+		return View::make('projects.ranking', compact($projectid));
 	}
 
 }
