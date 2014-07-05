98b34cdb59e4708bac35064ef845a76864259b3e
d3orn <dominique.rahm@gmail.com>
2014-07-05 13:35:45 +0200

    s5

--- website/hikomsys/app/views/projects/index.blade.php
+++ website/hikomsys/app/views/projects/index.blade.php
@@ -24,7 +24,8 @@
 								<td>{{ $project->name }}</td>
 								<td>{{ $project->version }}</td>
 								<td>{{ HTML::linkRoute('projects.show', 'Start new Quiz', [$project->project_id], ['class'=>'small button radius'])}}
-								{{ HTML::linkRoute('quizzes.index', 'Solutions', ['project_id' => $project->project_id], ['class'=>'success small button radius']) }}</td>
+								{{ HTML::linkRoute('quizzes.index', 'Solutions', ['project_id' => $project->project_id], ['class'=>'success small button radius']) }}{{ HTML::linkRoute('#', 'Ranking', ['project_id' => $project->project_id], ['class'=>'info small button radius']) }}
+								</td>
 							</tr>
 						@endforeach
 					</tbody>
