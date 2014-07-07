d57922334b135cc8b5436986fe23fa3da136e21d
Dominique Rahm <dominique.rahm@gmail.com>
2014-07-07 14:44:41 +0200

    s27

--- website/hikomsys/app/controllers/ProjectsController.php
+++ website/hikomsys/app/controllers/ProjectsController.php
@@ -155,25 +155,23 @@ class ProjectsController extends BaseController {
 	}
 
 	public function ranking($projectid){
-		var_dump(gettype($projectid));
 		/*	Select id, x.user_id, max From hikomsys.quizzes x 
 			Left outer join
 			(SELECT user_id, project_id, max(total_points) as max 
 			FROM hikomsys.quizzes Where project_id = 1 group by project_id, user_id) y
 			on x.total_points = y.max Where y.project_id is not null;*/
-		if(is_int($projectid)){
-			$ranking = DB::select('	SELECT id, x.user_id, max 
-									FROM quizzes x 
-									Left outer join
-									(SELECT user_id, project_id, max(total_points) as max 
-									FROM hikomsys.quizzes WHERE project_id = ' +$projectid+ ' 
-									GROUP BY project_id, user_id) y
-									ON x.total_points = y.max WHERE y.project_id is not null;');
+		$ranking = DB::select('	SELECT id, x.user_id, max 
+								FROM quizzes x 
+								Left outer join
+								(SELECT user_id, project_id, max(total_points) as max 
+								FROM hikomsys.quizzes WHERE project_id = ' +intval($projectid)+ ' 
+								GROUP BY project_id, user_id) y
+								ON x.total_points = y.max WHERE y.project_id is not null;');
 
-			return View::make('projects.ranking')
-				->with('ranking' , $ranking)
-				->with();
-		}
+		return View::make('projects.ranking')
+			->with('ranking' , $ranking)
+			->with();
+		
 	}
 
 }
