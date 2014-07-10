249d0f0efdbde4a5a877831afb8a10b05d4e6a1d
Dominique Rahm <dominique.rahm@gmail.com>
2014-07-10 14:29:26 +0200

    rv14

--- website/hikomsys/app/views/quizzes/result.blade.php
+++ website/hikomsys/app/views/quizzes/result.blade.php
@@ -39,6 +39,10 @@
 			<dl class="sub-nav">
 			  <dt>Functions:</dt>
 			  <dd class=""><a id="move" aria-hidden="true" data-icon="&#xe601;" class="icon-expand btn"></a></dd>
+			  <dd class="active"><a id="greenArrow" aria-hidden="true" data-icon="&#xe600;" class="icon-arrow-right btn arrowbtn"></a></dd>
+			  <dd class="active"><a id="orangeArrow" aria-hidden="true" data-icon="&#xe600;" class="icon-arrow-right btn arrowbtn"></a></dd>
+			  <dd class="active"><a id="redArrow" aria-hidden="true" data-icon="&#xe600;" class="icon-arrow-right btn arrowbtn"></a></dd>
+			  <dd id="info" class=""><a id="infosEnabled" class="btn">Additional Information</a></dd>
 			</dl>
 		</div>
 		<div class="medium-4 columns">	
