97b7128bc0bc2e9520660861289c14b06dcf4812
Dominique Rahm <dominique.rahm@gmail.com>
2014-10-11 15:15:32 +0200

    rv61

--- website/hikomsys/public/javascripts/packageViewer.js
+++ website/hikomsys/public/javascripts/packageViewer.js
@@ -33,7 +33,19 @@ $(document).ready(function(){
 
 		},
 		"plugins" : ["checkbox", "sort", "state", "wholerow", "contextmenu"]
-	}).bind('loaded.jstree', function (e, data) {
+	});
+
+	$("#package_list").jstree(true).clear_state();
+	$("#package_list").bind('loaded.jstree', function (e, data) {
+		alert("loaded");
+		var depth = 3;
+		data.inst.get_container().find('li').each(function (i) {
+			if (data.inst.get_path($(this)).length <= depth) {
+				data.inst.open_node($(this));
+			}
+		});
+	});	
+	$("#package_list").bind('ready.jstree', function (e, data) {
 		alert("loaded");
 		var depth = 3;
 		data.inst.get_container().find('li').each(function (i) {
@@ -42,9 +54,6 @@ $(document).ready(function(){
 			}
 		});
 	});
-
-	$("#package_list").jstree(true).clear_state();
-
 
 
 });
