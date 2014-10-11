02ba720bb0dd2a20fb994ed6f9eb4401423dc5ff
Dominique Rahm <dominique.rahm@gmail.com>
2014-10-11 12:35:44 +0200

    rv56

--- website/hikomsys/public/javascripts/packageViewer.js
+++ website/hikomsys/public/javascripts/packageViewer.js
@@ -58,7 +58,8 @@ $("#package_list").jstree({options}).bind('loaded.jstree', function (e, data) {
         if (data.inst.get_path($(this)).length <= depth) {
             data.inst.open_node($(this));
         }
-    }
+    
+    });
     });
 
 function closeAndDeselectChildren(id){
