176675caa03b6fff08d246366a3bb70524ce635d
d3orn <dr@oxon.ch>
2014-11-10 22:57:06 +0100

    rv148

--- website/hikomsys/app/database/seeds/UserTableSeeder.php
+++ website/hikomsys/app/database/seeds/UserTableSeeder.php
@@ -7,11 +7,46 @@ class UserTableSeeder extends Seeder {
         DB::table('users')->delete();
 
         User::create([
-        	'firstname' => 'Dominique', 
-        	'lastname' => 'Rahm', 
-        	'email' => 'd@r.com', 
-        	'password' => Hash::make('dddddd'),
-        	'username' => 'd3orn',
+            'firstname' => 'Dominique',
+            'lastname' => 'Rahm',
+            'email' => 'd@r.com',
+            'password' => Hash::make('dddddd'),
+            'username' => 'd3orn',
+            'picturepath' => 'img/dummy_picture.jpg'
+        ]);
+
+        User::create([
+            'firstname' => 'Haidar',
+            'lastname' => '',
+            'email' => 'haidar@ese.com',
+            'password' => Hash::make('98769876'),
+            'username' => '',
+            'picturepath' => 'img/dummy_picture.jpg'
+        ]);
+
+        User::create([
+            'firstname' => 'Bledar',
+            'lastname' => '',
+            'email' => 'bledar@ese.com',
+            'password' => Hash::make('98769876'),
+            'username' => '',
+            'picturepath' => 'img/dummy_picture.jpg'
+        ]);
+
+        User::create([
+            'lastname' => '',
+            'email' => 'andrea@ese.com',
+            'password' => Hash::make('98769876'),
+            'username' => '',
+            'picturepath' => 'img/dummy_picture.jpg'
+        ]);
+
+        User::create([
+        	'firstname' => 'Mircea',
+        	'lastname' => '',
+        	'email' => 'mircea@ese.com',
+        	'password' => Hash::make('98769876'),
+        	'username' => '',
             'picturepath' => 'img/dummy_picture.jpg'
         ]);
     }
